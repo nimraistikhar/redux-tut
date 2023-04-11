1c167e2b5d6afcbfcd1f97f039847563241799e5
nimra <nimra@myyogateacher.com>
2023-04-11 14:41:29 +0500

    Define constants, actions, reducers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import HomeContainer from './container/HomeContainer'
-import Home from './component/Home'
 
 function App() {
   return (
     <div className="App">
       <HomeContainer />
-      <Home />
     </div>
   );
 }
--- src/component/Home.js
+++ src/component/Home.js
@@ -1,26 +1,32 @@
 import React from 'react'
 
-const Home = () => {
-  return (
-    <div>
-        <div className="add-to-cart">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS6HKr_wUOpNfCEuHG9FcMYEZpOZmNJ4PwIg&usqp=CAU" alt="cart" />
+const Home = (props) => {
+    console.log("home", props.data)
+    return (
+        <div>
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_lMsw57hb_77VEF_leozZr91mPFlnttakbNDSh57rSG9ckRnsYHTGf1uiPq4xqZeP_IU&usqp=CAU" alt="cart" />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        I-Phone
+                    </span>
+                    <span>
+                        Price: $1000.00
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button
+                        onClick={
+                            () => { props.addToCartHandler({ pice: 1000, name: 'i phone 11' }) }
+                        }>
+                        Add To Cart</button>
+                </div>
+            </div>
         </div>
-        <h1>Home Component</h1>
-        <div className="cart-wrapper">
-            <div className="img-wrapper item">
-                <img src="https://freesvg.org/img/1527145993.png" alt="mobile" />
-            </div>
-            <div className="text-wrapper item">
-                <span>I-phone</span>
-                <span>Price: $1000.00</span>
-            </div>
-            <div className="btn-wrapper item">
-                <button>Add cart</button>
-            </div>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Home
